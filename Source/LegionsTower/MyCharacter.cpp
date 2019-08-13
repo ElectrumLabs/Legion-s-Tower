ca50b388fd15d3e8dc16b209af04252155bff688
Brian Bui <iambrianbui@yahoo.com>
2019-08-12 20:23:33 -0700

    might need help

--- Source/LegionsTower/MyCharacter.cpp
+++ Source/LegionsTower/MyCharacter.cpp
@@ -2,6 +2,7 @@
 
 
 #include "MyCharacter.h"
+#include "AbilitySystemComponent.h"
 
 // Sets default values
 AMyCharacter::AMyCharacter()
@@ -9,13 +10,22 @@ AMyCharacter::AMyCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	AbilitySystem = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystem"));
+
 }
 
 // Called when the game starts or when spawned
 void AMyCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	if (AbilitySystem)
+	{
+		if (HasAuthority() && Ability)
+		{
+			AbilitySystem->GiveAbility(FGameplayAbilitySpec(Ability.GetDefaultObject(), 1, 0));
+		}
+		AbilitySystem->InitAbilityActorInfo(this, this);
+	}
 }
 
 // Called every frame
@@ -28,7 +38,15 @@ void AMyCharacter::Tick(float DeltaTime)
 // Called to bind functionality to input
 void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
+	check(PlayerInputComponent);
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	AbilitySystem->BindAbilityActivationToInputComponent(PlayerInputComponent, FGameplayAbilityInputBinds("ConfirmInput", "CancelInput", "AbilityInput"));
 
 }
 
+void AMyCharacter::PossessedBy(AController * NewController) {
+	Super::PossessedBy(NewController);
+
+	AbilitySystem->RefreshAbilityActorInfo();
+}
--- Source/LegionsTower/MyCharacter.h
+++ Source/LegionsTower/MyCharacter.h
@@ -4,16 +4,30 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h" //We add this include
 #include "MyCharacter.generated.h"
 
-UCLASS()
-class LEGIONSTOWER_API AMyCharacter : public ACharacter
+UCLASS(config=Game)
+class LEGIONSTOWER_API AMyCharacter : public ACharacter, public IAbilitySystemInterface
 {
+
+	UAbilitySystemComponent* GetAbilitySystemComponent() const override {
+		return AbilitySystem;
+	}
+
 	GENERATED_BODY()
+
+	/** Our ability system */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Abilities, meta = (AllowPrivateAccess = "true"))
+	class UAbilitySystemComponent* AbilitySystem;
+
 
 public:
 	// Sets default values for this character's properties
 	AMyCharacter();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Abilities)
+		TSubclassOf<class UGameplayAbility> Ability;
 
 protected:
 	// Called when the game starts or when spawned
@@ -26,4 +40,20 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	virtual void PossessedBy(AController* NewController) override;
+
+
 };
+
+//It's very important that this enum is UENUM, because the code will look for UENUM by the given name and crash if the UENUM can't be found. BlueprintType is there so we can use these in blueprints, too. Just in case. Can be neat to define ability packages.
+UENUM(BlueprintType)
+enum class AbilityInput : uint8
+{
+	UseAbility1 UMETA(DisplayName = "Use Spell 1"), //This maps the first ability(input ID should be 0 in int) to the action mapping(which you define in the project settings) by the name of "UseAbility1". "Use Spell 1" is the blueprint name of the element.
+	UseAbility2 UMETA(DisplayName = "Use Spell 2"), //Maps ability 2(input ID 1) to action mapping UseAbility2. "Use Spell 2" is mostly used for when the enum is a blueprint variable.
+	UseAbility3 UMETA(DisplayName = "Use Spell 3"),
+	UseAbility4 UMETA(DisplayName = "Use Spell 4"),
+	WeaponAbility UMETA(DisplayName = "Use Weapon"), //This finally maps the fifth ability(here designated to be your weaponability, or auto-attack, or whatever) to action mapping "WeaponAbility".
+
+};
+
