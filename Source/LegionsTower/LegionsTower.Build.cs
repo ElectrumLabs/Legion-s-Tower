1cb6ab1264acb743541766f2092ff0f023767465
Brian Bui <iambrianbui@yahoo.com>
2019-08-12 21:59:07 -0700

    Kinda works

--- Source/LegionsTower/LegionsTower.Build.cs
+++ Source/LegionsTower/LegionsTower.Build.cs
@@ -8,7 +8,7 @@ public class LegionsTower : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayAbilities" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayAbilities", "GameplayTags" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
