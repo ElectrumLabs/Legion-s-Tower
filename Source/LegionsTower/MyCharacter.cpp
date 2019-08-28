b875bfb617d0c6d13da2fc6ea7a538c6ab263748
Randy Le <97randy.le@gmail.com>
2019-08-28 10:11:13 -0700

    Revert "checkpoint 2"
    This reverts commit e2839015423f5e28cf99d4c14d4dafb6a6d15363.

--- Source/LegionsTower/MyCharacter.cpp
+++ Source/LegionsTower/MyCharacter.cpp
@@ -3,10 +3,6 @@
 
 #include "MyCharacter.h"
 #include "AbilitySystemComponent.h"
-#include "AttributeSet.h"
-#include "GameplayAbility.h"
-
-UAbilitySystemComponent* MyCharacter::GetAbilitySystemComponent() const { return AbilitySystem;  }
 
 // Sets default values
 AMyCharacter::AMyCharacter()
@@ -16,24 +12,19 @@ AMyCharacter::AMyCharacter()
 
 	AbilitySystem = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystem"));
 
-	// Create the attribute set, the attribute is set up to communicate with the AbilitySystemComponent Automatically
-	AttributeSet = CreateDefaultSubobject<LTAttributeSet>(TEXT("AttributeSet"));
-
-	CharacterLevel = 1;
-
 }
 
 // Called when the game starts or when spawned
 void AMyCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-
-	AddDefaultGameplayAbilities();
-
-	/** Initialize the AttributeSet with the Database given */
-	if (AbilitySystem && AttrDataTable)
+	if (AbilitySystem)
 	{
-		const UAttributeSet* Attr = AbilitySystem->InitStats(LTAttributeSet::StaticClass(), AttrDataTable);
+		if (HasAuthority() && Ability)
+		{
+			AbilitySystem->GiveAbility(FGameplayAbilitySpec(Ability.GetDefaultObject(), 1, 0));
+		}
+		AbilitySystem->InitAbilityActorInfo(this, this);
 	}
 }
 
@@ -61,122 +52,3 @@ void AMyCharacter::PossessedBy(AController * NewController) {
 }
 
 //  new stuff
-
-void LTCharacter::PostInitializeComponents()
-{
-	Super::PostInitializeComponents();
-}
-
-void LTCharacter::RefreshSlottedAbilities()
-{
-	RemoveSlottedAbilities();
-	AddSlottedAbilities();
-}
-
-void LTCharacter::AddDefaultGameplayAbilities()
-{
-	AddSlottedAbilities();
-}
-
-void LTCharacter::RemoveSlottedAbilities()
-{
-	for (TPair<int32, FGameplayAbilitySpecHandle> SpecHandle : SlottedAbilities)
-	{
-		AbilitySystem->ClearAbility(SpecHandle.Value);
-		SlottedAbilities.Remove(SpecHandle.Key);
-	}
-}
-
-void LTCharacter::AddSlottedAbilities()
-{
-	TMap<int32, FGameplayAbilitySpec> SlottedAbilitySpecs;
-	FillSlottedAbilitySpecs(SlottedAbilitySpecs);
-
-	/** now adding the abilities onto the ability system and also keeping track of the SpecHandles */
-	for (TPair<int32, FGameplayAbilitySpec>& SpecPair : SlottedAbilitySpecs)
-	{
-		FGameplayAbilitySpecHandle &SpecHandle = SlottedAbilities.FindOrAdd(SpecPair.Key);
-
-		/* Check if it is not valid, it means that their is no skill or invalid skill */
-		if (!SpecHandle.IsValid())
-		{
-			SpecHandle = AbilitySystem->GiveAbility(SpecPair.Value);
-		}
-
-	}
-}
-
-void LTCharacter::FillSlottedAbilitySpecs(TMap<int32, FGameplayAbilitySpec>& SlottedAbilitySpecs)
-{
-	if (AbilitySystem)
-	{
-		if (HasAuthority() && AbilityOne)
-		{
-			SlottedAbilitySpecs.Add(
-				0,
-				FGameplayAbilitySpec(AbilityOne.GetDefaultObject(), 1, 0)
-			);
-		}
-		if (HasAuthority() && AbilityTwo)
-		{
-			SlottedAbilitySpecs.Add(
-				1,
-				FGameplayAbilitySpec(AbilityTwo.GetDefaultObject(), 1, 1)
-			);
-		}
-		if (HasAuthority() && AbilityThree)
-		{
-			SlottedAbilitySpecs.Add(
-				2,
-				FGameplayAbilitySpec(AbilityThree.GetDefaultObject(), 1, 2)
-			);
-		}
-		if (HasAuthority() && AbilityFour)
-		{
-			SlottedAbilitySpecs.Add(
-				3,
-				FGameplayAbilitySpec(AbilityFour.GetDefaultObject(), 1, 3)
-			);
-		}
-	}
-}
-
-void LTCharacter::HandleHealthChanged(float DeltaValue, const FGameplayTagContainer& EventTags)
-{
-	OnHealthChanged(DeltaValue, EventTags);
-}
-
-float LTCharacter::GetHealth() const
-{
-	return AttributeSet->GetHealth();
-}
-
-float LTCharacter::GetMaxHealth() const
-{
-	return AttributeSet->GetMaxHealth();
-}
-
-float LTCharacter::GetHealthRegenPerSecond() const
-{
-	return AttributeSet->GetHealthRegenPerSecond();
-}
-
-float LTCharacter::GetMana() const
-{
-	return AttributeSet->GetMana();
-}
-
-float LTCharacter::GetMaxMana() const
-{
-	return AttributeSet->GetMaxMana();
-}
-
-float LTCharacter::GetManaRegenPerSecond() const
-{
-	return AttributeSet->GetManaRegenPerSecond();
-}
-
-int32 LTCharacter::GetCharacterLevel() const
-{
-	return CharacterLevel;
-}
